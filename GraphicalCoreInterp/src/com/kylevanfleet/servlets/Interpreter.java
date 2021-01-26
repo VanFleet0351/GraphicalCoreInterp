c1222f81f4616f3cd08f0ca789018ced54c3e299
VanFleet0351 <50758972+VanFleet0351@users.noreply.github.com>
2021-01-26 17:43:49 -0500

    Connected interface with backend processes. Needs to be Scanner needs to be adapted to using StringReader over the FileReader. StringReader returns '-1' at the end of the String.

--- GraphicalCoreInterp/src/com/kylevanfleet/servlets/Interpreter.java
+++ GraphicalCoreInterp/src/com/kylevanfleet/servlets/Interpreter.java
@@ -1,11 +1,16 @@
 package com.kylevanfleet.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kylevanfleet.interpreter.Parser;
+import com.kylevanfleet.interpreter.Scanner;
 
 /**
  * Servlet implementation class Interpreter
@@ -34,8 +39,26 @@ public class Interpreter extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		String prog = request.getParameter("prog").trim();
+		String data = request.getParameter("data").trim();
+		out.println(prog);
+		out.println(data);
+		
+		
+		try {
+			Scanner coreScanner = new Scanner(prog, data);
+			Parser coreParser = new Parser(coreScanner);
+			String prettyCode = coreParser.printCode();
+			String output = coreParser.execute();
+			out.println(prettyCode);
+			out.println("<br>");
+			out.println(output);
+		} catch (Exception e) {
+			out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 }
